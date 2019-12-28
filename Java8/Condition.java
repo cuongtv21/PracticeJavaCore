c6484a5975d7397c7f83416034c692769488c9cc
cuongtv21 <trancuong8295@gmail.com>
2019-12-28 23:27:07 +0700

    practices java 8

---
+++ Java8/Condition.java
@@ -0,0 +1,5 @@
+package fa.edu.javacore.java8;
+
+public enum Condition {
+    GOOD, BAD
+}
---
+++ Java8/ISBN.java
@@ -0,0 +1,20 @@
+package fa.edu.javacore.java8;
+
+public class ISBN {
+
+    private String isbn10;
+    private String isbn13;
+
+    public ISBN(Book book) {
+        isbn10 = ("0" + Math.abs(book.getTitle().hashCode())).substring(0, 10);
+        isbn13 = "978" + isbn10;
+    }
+
+    @Override
+    public String toString() {
+        return "ISBN{" +
+                "isbn10='" + isbn10 + '\'' +
+                ", isbn13='" + isbn13 + '\'' +
+                '}';
+    }
+}
